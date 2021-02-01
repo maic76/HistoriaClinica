d2acdc9dea65823fa98806e0a8020889e5049ea4
DESKTOP-D94GMU7\mike <maic76@gmail.com>
2021-02-01 10:33:21 -0600

    commit inicial, creando Layout principal

--- app/src/main/java/mx/lania/historiaclinica/FichaId.java
+++ app/src/main/java/mx/lania/historiaclinica/FichaId.java
@@ -7,32 +7,36 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class FichaId extends AppCompatActivity {
 
     String tag = "Loggin";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_ficha_id);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
         //actionBar.hide();
-        //actionBar.setDisplayHomeAsUpEnabled(true);
-
-
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
     }
 
 
 
-    public void onClick(View v){
-        //Log.d(tag,"entre al clcik");
-        startActivity(new Intent("mx.lania.usingintent.FichaId"));
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        Log.d(tag,"click al boton de la app");
+        Intent i = new Intent(getBaseContext(), MainActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(i);
+        return true;
     }
 
 
